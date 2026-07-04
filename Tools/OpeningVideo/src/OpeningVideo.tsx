9f1163ba3188fe377de9e0a854cc42ad268625ab
llluooo <jerry_luo0518@126.com>
2026-07-04 17:45:11 +0800

    chore: harden opening video project shell

---
+++ Tools/OpeningVideo/src/OpeningVideo.tsx
@@ -0,0 +1,5 @@
+import { AbsoluteFill } from "remotion";
+
+export const OpeningVideo = () => {
+  return <AbsoluteFill style={{ backgroundColor: "#05060a" }} />;
+};
---
+++ Tools/OpeningVideo/src/openingConfig.ts
@@ -0,0 +1,6 @@
+export const composition = {
+  width: 1920,
+  height: 1080,
+  fps: 30,
+  durationInFrames: 450,
+} as const;
